48e59fbeb58f9c25b9d4cbe483fc48ddbe9079b6
Sorethea OK <sorethea@gmail.com>
2024-05-03 15:11:12 +0700

    add employee seeder

---
+++ src/Seeders/EmployeeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sorethea\Hrms\Seeders;
+
+use Illuminate\Database\Seeder;
+use Sorethea\Hrms\Models\Employee;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Employee::newFactory(1)->create();
+    }
+}
