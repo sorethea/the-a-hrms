fc31614fe83a861f39695744ece529e7cc093ffd
Sorethea OK <sorethea@gmail.com>
2024-05-03 15:55:41 +0700

    add employee seeder

--- src/HrmsServiceProvider.php
+++ src/HrmsServiceProvider.php
@@ -12,12 +12,17 @@ class HrmsServiceProvider extends PackageServiceProvider
     public static string $name = "the-a-hrms";
     public function register(): void
     {
-
+        if (! app()->configurationIsCached()) {
+            $this->mergeConfigFrom(__DIR__.'/../config/hrms.php', 'hrms');
+        }
     }
 
     public function boot(): void
     {
         $this->publishesMigrations([__DIR__.'/../database/migrations'=>database_path('migrations')],'the-a-hrms-migrations');
+        $this->publishes([
+            __DIR__.'/../config/hrms.php' => config_path('hrms.php'),
+        ], 'hrms-config');
     }
 
     public function configurePackage(Package $package): void
