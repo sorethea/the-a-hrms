a46347f166bc373541c0ce018b68fd5809c54578
Sorethea OK <sorethea@gmail.com>
2024-05-03 15:23:36 +0700

    add employee seeder

--- src/Seeders/EmployeeSeeder.php
+++ src/Seeders/EmployeeSeeder.php
@@ -12,6 +12,6 @@ class EmployeeSeeder extends Seeder
      */
     public function run(): void
     {
-        Employee::newFactory(1000)->create();
+        Employee::factory(1000)->create();
     }
 }
